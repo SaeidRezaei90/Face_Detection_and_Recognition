f76a9e96f04eba6efecf2cfaaf3d26fc089c5438
Saeid <saeid69.rezaei@gmail.com>
2020-03-28 22:37:35 +0000

    Done prepare_training_data Function

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def detect_face(img):
     #result is a list of faces(rect parameters)
     faces = face_cascade.detectMultiScale(gray_image,scaleFactor =1.2,minNeighbours=5)
 
-    #if no faces are detected then return original image
+    #if no faces are detected then return original image, we return two None value because detect face fun returns two values
     if (len(faces) == 0):
       return None, None
+
 
     #under the assumption that there is only one face , then extract the face parameters
     (x, y, w, h) = faces[0]
@@ -39,7 +40,46 @@ def prepare_training_data(image_folder_path):
 
   #go throgh each directory and read the images
   for dir in dirs:
-    #we know that our subject directory 
+    #we know that our subject directories starts with 's', so we first ignore any none relevant directories
+    if not dir.startwith("s"):
+      continue
+    
+  #remove letter s from the directory name will give a lable and add the lable to the lable list
+  lable =int(dir.replace("s",""))
+  lables.append(lable)
+
+  subject_dir_path = image_folder_path + "/" + dir #e.g. ~/s1
+
+  #get all the images name in the subject directory
+  subject_image_names = os.listdir(subject_dir_path)
+
+  for image_name in subject_image_names:
+    #first ignore system files like .ds_Store
+    if image_name.startwith("."):
+      continue
+    
+    #build image path
+    image_path = subject_dir_path + "/" + image_name
+
+    #read the image and display an image window to show the pic
+    image = cv2.imread(image_path)
+    cv2.imshow("Training on image ...", cv2.resize(image,(400,500)))
+    cv2.waitkey(100)
+
+    #pass the image to detect face fun
+    face, rect = detect_face(image)
+
+    #we should ignore faces that are not detected
+    if face is not None:
+      #add face to the faces list
+      faces.append(face)
+  
+  cv2.destroyAllWindows()
+  cv2.waitKey(1)
+  cv2.destroyAllWindows()
+  
+print("End")    
+
 
 
   
